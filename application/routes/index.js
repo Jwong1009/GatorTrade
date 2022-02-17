b68e63c77c8f3edcb518a3500ddb9628bffbe035
faisal-zaheervi <faisalszaheer@gmail.com>
2022-02-16 19:42:20 -0800

    Created Express/Node app, refactored Zaheer, Patel, Wong and Home pages to EJS engine

---
+++ application/routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Team 05 Home Page' });
+});
+
+router.get('/about/patel', function(req, res, next) {
+  res.render('about_patel');
+});
+
+router.get('/about/lei', function(req, res, next) {
+  res.render('about_lei');
+});
+
+router.get('/about/zaheer', function(req, res, next) {
+  res.render('about_zaheer');
+});
+
+router.get('/about/hernandez', function(req, res, next) {
+  res.render('about_hernandez');
+});
+
+router.get('/about/wong', function(req, res, next) {
+  res.render('about_wong');
+});
+
+router.get('/about/cheung', function(req, res, next) {
+  res.render('about_cheung');
+});
+
+module.exports = router;
