48425c79feb960d0ac680a7098833ce74793e2e2
Ze Lei <57436044+Frogger-Software@users.noreply.github.com>
2022-03-17 13:44:06 -0700

    update database files

---
+++ application/db/GatorTrade.sql
@@ -0,0 +1,137 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema GatorTrade
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `GatorTrade` ;
+
+-- -----------------------------------------------------
+-- Schema GatorTrade
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `GatorTrade` DEFAULT CHARACTER SET utf8 ;
+USE `GatorTrade` ;
+
+-- -----------------------------------------------------
+-- Table `GatorTrade`.`Categories`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `GatorTrade`.`Categories` ;
+
+CREATE TABLE IF NOT EXISTS `GatorTrade`.`Categories` (
+  `idCategories` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idCategories`),
+  UNIQUE INDEX `idCategories_UNIQUE` (`idCategories` ASC),
+  UNIQUE INDEX `name_UNIQUE` (`title` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `GatorTrade`.`Users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `GatorTrade`.`Users` ;
+
+CREATE TABLE IF NOT EXISTS `GatorTrade`.`Users` (
+  `idUsers` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `password` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `firstname` VARCHAR(45) NULL DEFAULT 'none',
+  `lastname` VARCHAR(45) NULL DEFAULT 'none',
+  `admin` TINYINT NULL DEFAULT 0,
+  PRIMARY KEY (`idUsers`),
+  UNIQUE INDEX `idUsers_UNIQUE` (`idUsers` ASC),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `GatorTrade`.`Items`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `GatorTrade`.`Items` ;
+
+CREATE TABLE IF NOT EXISTS `GatorTrade`.`Items` (
+  `idItems` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `category` INT UNSIGNED NOT NULL,
+  `seller` INT UNSIGNED NOT NULL,
+  `photopath` VARCHAR(4096) NULL DEFAULT 'none',
+  `description` VARCHAR(128) NULL DEFAULT 'none',
+  `title` VARCHAR(45) NULL DEFAULT 'none',
+  `price` DOUBLE UNSIGNED NULL,
+  `approved` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`idItems`),
+  UNIQUE INDEX `idItems_UNIQUE` (`idItems` ASC),
+  INDEX `ITEMS_CATEGORIES_FK_idx` (`category` ASC),
+  INDEX `ITEMS_USERS_FK_idx` (`seller` ASC),
+  CONSTRAINT `ITEMS_CATEGORIES_FK`
+    FOREIGN KEY (`category`)
+    REFERENCES `GatorTrade`.`Categories` (`idCategories`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `ITEMS_USERS_FK`
+    FOREIGN KEY (`seller`)
+    REFERENCES `GatorTrade`.`Users` (`idUsers`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `GatorTrade`.`Reviews`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `GatorTrade`.`Reviews` ;
+
+CREATE TABLE IF NOT EXISTS `GatorTrade`.`Reviews` (
+  `idReviews` INT NOT NULL,
+  `reviewer` INT UNSIGNED NOT NULL,
+  `reviewee` INT UNSIGNED NOT NULL,
+  `rating` TINYINT(5) UNSIGNED NOT NULL,
+  `header` VARCHAR(45) NULL,
+  `body` VARCHAR(128) NULL,
+  PRIMARY KEY (`idReviews`),
+  UNIQUE INDEX `idReviews_UNIQUE` (`idReviews` ASC),
+  INDEX `REVIEWS_USERS_FK_idx` (`reviewer` ASC),
+  INDEX `REVIEWS_USERS_FK_idx1` (`reviewee` ASC),
+  CONSTRAINT `REVIEWEES_USERS_FK`
+    FOREIGN KEY (`reviewer`)
+    REFERENCES `GatorTrade`.`Users` (`idUsers`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `REVIEWERS_USERS_FK`
+    FOREIGN KEY (`reviewee`)
+    REFERENCES `GatorTrade`.`Users` (`idUsers`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `GatorTrade`.`Messages`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `GatorTrade`.`Messages` ;
+
+CREATE TABLE IF NOT EXISTS `GatorTrade`.`Messages` (
+  `idMessages` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `messager` INT UNSIGNED NOT NULL,
+  `messagee` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`idMessages`),
+  INDEX `MESSAGER_USERS_FK_idx` (`messager` ASC),
+  INDEX `MESSAGEE_USERS_FK_idx` (`messagee` ASC),
+  CONSTRAINT `MESSAGER_USERS_FK`
+    FOREIGN KEY (`messager`)
+    REFERENCES `GatorTrade`.`Users` (`idUsers`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `MESSAGEE_USERS_FK`
+    FOREIGN KEY (`messagee`)
+    REFERENCES `GatorTrade`.`Users` (`idUsers`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
