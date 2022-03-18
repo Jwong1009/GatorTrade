314e5c5a9afbda049a67a32a92f9b217aafaf37b
Ze Lei <zlei1@mail.sfsu.edu>
2022-03-18 10:49:10 -0700

    debugging server

--- application/db/index.js
+++ application/db/index.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root', // Insert your MySQL username here
     password: 'Team05Guest!', // Insert your MySQL password here
-    database: 'gatortrade', 
+    database: 'GatorTrade', 
     connectionLimit: 50,
     debug: false
 });
