67277f2070b47488191aec17c93aff7a1deeac7d
Faisal Zaheer <faisalszaheer@gmail.com>
2022-03-17 19:02:31 -0700

    Update index.js

--- application/db/index.js
+++ application/db/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root', // Insert your MySQL username here
-    password: 'student', // Insert your MySQL password here
+    password: 'Team05Guest!', // Insert your MySQL password here
     database: 'gatortrade', 
     connectionLimit: 50,
     debug: false
