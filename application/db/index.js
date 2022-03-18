71d1a8a887e812c69e58580e071746a7dd8db677
Ze Lei <zlei1@mail.sfsu.edu>
2022-03-18 14:06:49 -0700

    finished debugging

--- application/db/index.js
+++ application/db/index.js
@@ -3,19 +3,19 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root', // Insert your MySQL username here
-    password: 'Team05Guest!', // Insert your MySQL password here
+    password: 'csc317', // Insert your MySQL password here
     database: 'GatorTrade', 
     connectionLimit: 50,
     debug: false
 });
 
-pool.query("SELECT * FROM Items WHERE title LIKE '' ORDER BY title;",(err, data) => {
-    if(err) {
-        console.error(err);
-        return;
-    }
-    // rows fetch
-    console.log(data);
-});
+// pool.query("SELECT * FROM Items WHERE title LIKE '' ORDER BY title;",(err, data) => {
+//     if(err) {
+//         console.error(err);
+//         return;
+//     }
+//     // rows fetch
+//     console.log(data);
+// });
 
 module.exports = pool;
--- application/routes/index.js
+++ application/routes/index.js
@@ -3,62 +3,62 @@ const db = require('../db');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Team 05 Home Page' });
 });
 
 // Test homepage with search bar
-router.get('/test_homepage', function(req, res, next) {
+router.get('/test_homepage', function (req, res, next) {
   res.render('test_homepage', { title: 'Team 05 Home Page' });
 });
 
-router.get('/test_results', function(req, res, next) {
+router.get('/test_results', function (req, res, next) {
   const { search, category } = req.query;
   let categoryId = parseInt(category);
   let totalItemCount = 0;
 
   // Gets total count of items in database to display on /test_results page.
   db.query('SELECT COUNT(*) AS length FROM Items;', (err, results, fields) => {
-    console.log(results[0].length);
     totalItemCount = results[0].length;
   });
 
   // Selected "All" for Category. No need to factor category into search.
-  if(categoryId == 0) {
+  if (categoryId == 0) {
     db.query(`SELECT * FROM Items WHERE title LIKE '${search}%' ORDER BY title;`, (err, results, fields) => {
-      res.render('test_results', { title: 'Team 05 Home Page', results: results, total: totalItemCount});
+      res.render('test_results', { title: 'Team 05 Home Page', results: results, total: totalItemCount });
+      // console.log(totalItemCount);
     });
   }
 
   // Filter results based on category chosen.
-  else if(categoryId > 0) {
+  else if (categoryId > 0) {
     db.query(`SELECT * FROM Items WHERE title LIKE '${search}%' AND category=${categoryId} ORDER BY title;`, (err, results, fields) => {
-      res.render('test_results', { title: 'Team 05 Home Page', results: results, total: totalItemCount});
+      res.render('test_results', { title: 'Team 05 Home Page', results: results, total: totalItemCount });
     });
   }
 });
 
-router.get('/about/patel', function(req, res, next) {
+router.get('/about/patel', function (req, res, next) {
   res.render('about_patel');
 });
 
-router.get('/about/lei', function(req, res, next) {
+router.get('/about/lei', function (req, res, next) {
   res.render('about_lei');
 });
 
-router.get('/about/zaheer', function(req, res, next) {
+router.get('/about/zaheer', function (req, res, next) {
   res.render('about_zaheer');
 });
 
-router.get('/about/hernandez', function(req, res, next) {
+router.get('/about/hernandez', function (req, res, next) {
   res.render('about_hernandez');
 });
 
-router.get('/about/wong', function(req, res, next) {
+router.get('/about/wong', function (req, res, next) {
   res.render('about_wong');
 });
 
-router.get('/about/cheung', function(req, res, next) {
+router.get('/about/cheung', function (req, res, next) {
   res.render('about_cheung');
 });
 
