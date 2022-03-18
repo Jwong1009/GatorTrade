b882cd74d8bfdec1abd814d6076fc23399065659
Ze Lei <zlei1@mail.sfsu.edu>
2022-03-18 14:09:30 -0700

    fix DB password

--- application/db/index.js
+++ application/db/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root', // Insert your MySQL username here
-    password: 'csc317', // Insert your MySQL password here
+    password: 'Team05Guest!', // Insert your MySQL password here
     database: 'GatorTrade', 
     connectionLimit: 50,
     debug: false
