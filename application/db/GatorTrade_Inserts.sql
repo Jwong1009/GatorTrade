324785a2da5bd486e013f40fbdbd342675c88410
Ze Lei <57436044+Frogger-Software@users.noreply.github.com>
2022-04-03 11:37:02 -0700

    made item descriptions longer

--- application/db/GatorTrade_Inserts.sql
+++ application/db/GatorTrade_Inserts.sql
@@ -8,32 +8,32 @@ INSERT INTO `GatorTrade`.`Users` (`password`, `email`, `firstname`, `lastname`) 
 INSERT INTO `GatorTrade`.`Users` (`password`, `email`, `firstname`, `lastname`, `admin`) VALUES 
 ("Password", "student@mail.sfsu.edu", "Joe", "Schmuck", 1);
 INSERT INTO `GatorTrade`.`Items` (`category`, `seller`, `description`, `title`, `price`, `photopath`) VALUES 
-(2, 1, "A fantasy book", "Lord of The Rings", 10, "public/images/lotr.jpg"),
- (2, 1, "A magical book", "Harry Potter", 10, "public/images/harry-potter.jpg"), 
- (2, 1, "A mythology book", "Percy Jackson", 10, "public/images/percy-jackson.jpg"), 
- (2, 1, "A book with a horrible movie", "Artemis Fowl", 10, "public/images/artemis-fowl.jpg"), 
- (2, 1, "A dystopian book", "Divergent", 10, "public/images/divergent.jpg");
+(2, 1, "A fantasy book nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Lord of The Rings", 10, "public/images/lotr.jpg"),
+ (2, 1, "A magical book nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Harry Potter", 10, "public/images/harry-potter.jpg"), 
+ (2, 1, "A mythology book nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Percy Jackson", 10, "public/images/percy-jackson.jpg"), 
+ (2, 1, "A book with a horrible movie nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Artemis Fowl", 10, "public/images/artemis-fowl.jpg"), 
+ (2, 1, "A dystopian book nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Divergent", 10, "public/images/divergent.jpg");
 INSERT INTO `GatorTrade`.`Items` (`category`, `seller`, `description`, `title`, `price`, `photopath`) VALUES 
-(1, 1, "Like New", "iPad", 200, "public/images/ipad.jpg"), 
-(1, 1, "Like New", "iPhone", 400, "public/images/iphone.jpg"), 
-(1, 1, "Like New", "Samsung S22", 400, "public/images/s22.jpg"), 
-(1, 1, "Like New", "Razer Orochi Mouse", 40, "public/images/orochi.jpg"), 
-(1, 1, "Like New", "Logitech Lightspeed Mouse", 10, "public/images/lightspeed.jpg");
+(1, 1, "Like New nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "iPad", 200, "public/images/ipad.jpg"), 
+(1, 1, "Like New nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "iPhone", 400, "public/images/iphone.jpg"), 
+(1, 1, "Like New nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Samsung S22", 400, "public/images/s22.jpg"), 
+(1, 1, "Like New nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Razer Orochi Mouse", 40, "public/images/orochi.jpg"), 
+(1, 1, "Like New nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Logitech Lightspeed Mouse", 10, "public/images/lightspeed.jpg");
 INSERT INTO `GatorTrade`.`Items` (`category`, `seller`, `description`, `title`, `price`, `photopath`) VALUES 
-(3, 1, "Cotton", "Crochet Plush", 10, "public/images/plush.jpg"), 
-(3, 1, "Cotton", "Hoodies", 10, "public/images/hoodie.jpg"), 
-(3, 1, "Leather", "Wallet", 10, "public/images/wallet.jpg"), 
-(3, 1, "Cotton", "Blanket", 10, "public/images/blanket.jpeg"), 
-(3, 1, "Silver", "Earrings", 10, "public/images/earrings.jpg");
+(3, 1, "Cotton nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Crochet Plush", 10, "public/images/plush.jpg"), 
+(3, 1, "Cotton nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Hoodies", 10, "public/images/hoodie.jpg"), 
+(3, 1, "Leather nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Wallet", 10, "public/images/wallet.jpg"), 
+(3, 1, "Cotton nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Blanket", 10, "public/images/blanket.jpeg"), 
+(3, 1, "Silver nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Earrings", 10, "public/images/earrings.jpg");
 INSERT INTO `GatorTrade`.`Items` (`category`, `seller`, `description`, `title`, `price`, `photopath`) VALUES 
-(4, 1, "Like New", "CSC648 Textbook", 10, "public/images/csc-textbook.jpg"), 
-(4, 1, "Like New", "CSC600 Textbook", 10, "public/images/csc-textbook.jpg"), 
-(4, 1, "Like New", "BIOL101 Textbook", 10, "public/images/biol-textbook.jpg"), 
-(4, 1, "Like New", "Lab Goggles", 10, "public/images/lab-goggles.jpg"), 
-(4, 1, "Like New", "Lab Coat", 10, "public/images/lab-coat.jpg");
+(4, 1, "Like New nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "CSC648 Textbook", 10, "public/images/csc-textbook.jpg"), 
+(4, 1, "Like New nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "CSC600 Textbook", 10, "public/images/csc-textbook.jpg"), 
+(4, 1, "Like New nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "BIOL101 Textbook", 10, "public/images/biol-textbook.jpg"), 
+(4, 1, "Like New nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Lab Goggles", 10, "public/images/lab-goggles.jpg"), 
+(4, 1, "Like New nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Lab Coat", 10, "public/images/lab-coat.jpg");
 INSERT INTO `GatorTrade`.`Items` (`category`, `seller`, `description`, `title`, `price`, `photopath`) VALUES 
-(5, 1, "Tastes Good", "Spaghetti", 10, "public/images/spaghetti.jpg"), 
-(5, 1, "Tastes Good", "Meatballs", 10, "public/images/meatballs.jpg"), 
-(5, 1, "Tastes Good", "Cupcakes", 10, "public/images/cupcakes.jpg"), 
-(5, 1, "Tastes Good", "Italian Coffee", 10, "public/images/coffee.jpg"), 
-(5, 1, "Tastes Good", "Apple Pie", 10, "public/images/apple-pie.jpg");
+(5, 1, "Tastes Good nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Spaghetti", 10, "public/images/spaghetti.jpg"), 
+(5, 1, "Tastes Good nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Meatballs", 10, "public/images/meatballs.jpg"), 
+(5, 1, "Tastes Good nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Cupcakes", 10, "public/images/cupcakes.jpg"), 
+(5, 1, "Tastes Good nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Italian Coffee", 10, "public/images/coffee.jpg"), 
+(5, 1, "Tastes Good nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes", "Apple Pie", 10, "public/images/apple-pie.jpg");
