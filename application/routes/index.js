f2fd6b178f32eefce77933dff6bf4f333898d92a
Ze Lei <zlei1@mail.sfsu.edu>
2022-04-10 23:27:12 -0700

    implemented messages

--- application/routes/index.js
+++ application/routes/index.js
@@ -96,12 +96,11 @@ router.get('/dp', function(req, res, next){
   const {id} = req.query;
   let idItems = parseInt(id);
   //Uses idItems column in Items table to filter out the row of data we want to display
-  db.query("SELECT * FROM Items WHERE idItems = ?", [idItems]).then(([Item])=>{
+  db.query("SELECT * FROM Items I LEFT JOIN Users U ON I.seller = U.idUsers WHERE idItems =  ?;", [idItems]).then(([Item])=>{
     res.render('itemsDetailPage', {title: "Team 05 item's detail page", Item: Item});
   }).catch(error =>{
     console.log(error);
   });
->>>>>>> Stashed changes
 });
 
 module.exports = router;
--- application/routes/messages.js
+++ application/routes/messages.js
@@ -12,33 +12,43 @@ const MessagesModel = require('../models/Messages');
 const { successPrint, errorPrint } = require('../helpers/debug/debugprinters');
 
 router.post('/send', function (req, res, next) {
-    // const {item, sender, receiver, body} = req.query;
-    const body = req.body.message;
-    MessagesModel.create('1', '1', '2', body)
-    .then((messageStatus) => {
-      if(messageStatus > 0) {
-        successPrint(`message sent`);
-        req.flash('success', 'Message sent');
-        req.session.save( err => {
-          res.redirect('/');
-        });
-      } else {
-        throw new MessagesError("Could not send message", "/message", 200);
-      }
-    })
-    .catch((err) => {
-      errorPrint("message send failed");
-      if (err instanceof MessagesError) {
-        errorPrint(err.getMessage());
-        req.flash('error', err.getMessage());
-        res.status(err.getStatus());
-        res.redirect(err.getRedirectURL());
-      } else {
-        // next(err);
-        res.redirect("/message")
-      }
+  if (!req.session.userId) {
+    errorPrint('must be logged in to message');
+    res.json({
+      code: -1,
+      status: 'danger',
+      message: 'must be logged in to create a message'
     });
-  });
+  } else {
+    const body = req.body.message;
+    const sender = req.session.userId;
+    const {item, receiver} = req.query;
+    MessagesModel.create(item, sender, receiver, body)
+      .then((messageStatus) => {
+        if (messageStatus > 0) {
+          successPrint(`message sent`);
+          req.flash('success', 'Message sent');
+          req.session.save(err => {
+            res.redirect('/');
+          });
+        } else {
+          throw new MessagesError("Could not send message", "/message", 200);
+        }
+      })
+      .catch((err) => {
+        errorPrint("message send failed");
+        if (err instanceof MessagesError) {
+          errorPrint(err.getMessage());
+          req.flash('error', err.getMessage());
+          res.status(err.getStatus());
+          res.redirect(err.getRedirectURL());
+        } else {
+          // next(err);
+          res.redirect("/message")
+        }
+      });
+  }
+});
 
-  
+
 module.exports = router;
