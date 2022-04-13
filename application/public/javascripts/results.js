2f687217afcd03502bce5cbbe82cd5aac1220a17
Kishan Patel <kishan@sfs-wifi-dhcp-10-143-78-76.wifi.sfsu.edu>
2022-04-13 14:39:48 -0700

    Added links to titles on item cards

--- application/public/javascripts/results.js
+++ application/public/javascripts/results.js
@@ -27,57 +27,69 @@ function compareValues(key, order = 'asc') {
 
 // Function for updating the sorted results on the results page.
 function updateResults() {
-resultsContainer.innerHTML = '';
-let updatedContent = '';
-let count = 0;
-for(item of results){
-    let categoryName;
-    switch(item.category){
-    case 1: categoryName = 'Electronics';
-            break;
-    case 2: categoryName = 'Textbooks';
-            break;
-    case 3: categoryName = 'Handicraft';
-            break;
-    case 4: categoryName = 'Education Supplies';
-            break;
-    case 5: categoryName = 'Food';
-            break;
-    default: break;
-    }
+    console.log('Running...');
+    resultsContainer.innerHTML = '';
+    let updatedContent = '';
+    let count = 0;
+    for(item of results){
+        let categoryName;
+        switch(item.category){
+        case 1: categoryName = 'Electronics';
+                break;
+        case 2: categoryName = 'Textbooks';
+                break;
+        case 3: categoryName = 'Handicraft';
+                break;
+        case 4: categoryName = 'Education Supplies';
+                break;
+        case 5: categoryName = 'Food';
+                break;
+        default: break;
+        }
 
-    let itemCard = `<div class="col-md-4">
-                    <div class="card h-100" style="width: 18rem;">
-                        <img src="${item.photopath}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title">${item.title}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${item.category}</h6>
-                        <p class="card-text">${item.description}</p>
-                        <span class="border border-primary rounded-2 px-4 pv-2 fs-5">$${item.price}</span>
+        let itemCard = `<div class="col-md-4">
+                        <div class="card h-100" style="width: 18rem;">
+                            <a href="/dp?id=${item.idItems}">
+                                <img src="${item.photopath}" class="card-img-top" alt="...">
+                            </a>
+                            <div class="card-body">
+                            <a href="/dp?id=${item.idItems}">
+                                <h5 class="card-title">${item.title}</h5>
+                            </a>
+                            <h6 class="card-subtitle mb-2 text-muted">${item.category}</h6>
+                            <p class="card-text">${item.description}</p>
+                            <span class="border border-primary rounded-2 px-4 pv-2 fs-5">$${item.price}</span>
+                            </div>
                         </div>
-                    </div>
-                    </div>`;
-    if(count % 3 === 0){
-        updatedContent += `<div class="row mb-5">` + itemCard;
-    } else if(count % 3 === 1){
-        updatedContent += itemCard;
-    } else if(count % 3 === 2 || count + 1 === results.length){
-        updatedContent += itemCard + `</div>`;
+                        </div>`;
+        if(count % 3 === 0){
+            updatedContent += `<div class="row mb-5">` + itemCard;
+        } else if(count % 3 === 1){
+            updatedContent += itemCard;
+        } else if(count % 3 === 2 || count + 1 === results.length){
+            updatedContent += itemCard + `</div>`;
+        }
+        count++;
     }
-    count++;
-}
-resultsContainer.innerHTML = updatedContent;
+    resultsContainer.innerHTML = updatedContent;
 }
 
-//Sorting the results according to the selected sort option.
-sortSelect.addEventListener("change", () => {
-if(sortSelect.value === 'default'){
-    var key = 'idItems';
-    var order = 'asc';
-}else{
-    var key = sortSelect.value.slice(0,5);
-    var order = sortSelect.value.slice(6);
+// Helper function to sort results according to the selected sort value.
+function sortResults(){
+    if(sortSelect.value === 'default'){
+        console.log('if...');
+        var key = 'idItems';
+        var order = 'asc';
+    }else{
+        console.log('else...');
+        var key = sortSelect.value.slice(0,5);
+        var order = sortSelect.value.slice(6);
+    }
+    results.sort(compareValues(key, order));
 }
-results.sort(compareValues(key, order));
-updateResults();
+
+//Sorting and displaying sorted results when the selected sort value changes.
+sortSelect.addEventListener("change", () => {
+    sortResults();
+    updateResults();
 });
