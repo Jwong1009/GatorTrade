38416b8559651c3b2fffc0b281c25e2684c85b14
Ze Lei <zlei1@mail.sfsu.edu>
2022-05-10 11:22:34 -0700

    hide items that have not been approved

--- application/db/GatorTrade_Inserts2.sql
+++ application/db/GatorTrade_Inserts2.sql
@@ -21,9 +21,9 @@ INSERT INTO `GatorTrade`.`Reviews` (`header`,`rating`,`reviewer`, `reviewee`,`bo
 ("Review No. 3", 2, 6 ,2,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do eiusmod tempor.");
 
 -- to test My Posts (by Peter)
-INSERT INTO `GatorTrade`.`Items` (`category`, `seller`, `description`, `title`, `price`, `photopath`, `thumbnail`) VALUES 
-(2, 6, "A fantasy book nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis", "Lord of The Rings", 11, "images/lotr.jpg", "images/thumbnails/thumbnail-lotr.jpeg"),
- (2, 6, "A magical book nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis", "Harry Potter", 12, "images/harry-potter.jpg", "images/thumbnails/thumbnail-harry-potter.jpeg"), 
- (2, 6, "A mythology book nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis", "Percy Jackson", 13, "images/percy-jackson.jpg", "images/thumbnails/thumbnail-percy-jackson.jpeg"), 
- (2, 6, "A book with a horrible movie nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis", "Artemis Fowl", 14, "images/artemis-fowl.jpg", "images/thumbnails/thumbnail-artemis-fowl.jpeg"), 
- (2, 6, "A dystopian book nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis", "Divergent", 15, "images/divergent.jpg", "images/thumbnails/thumbnail-divergent.jpeg");
+INSERT INTO `GatorTrade`.`Items` (`category`, `seller`, `description`, `title`, `price`, `photopath`, `thumbnail`, `approved`) VALUES 
+(2, 6, "A fantasy book nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis", "Lord of The Rings", 11, "images/lotr.jpg", "images/thumbnails/thumbnail-lotr.jpeg", 1),
+ (2, 6, "A magical book nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis", "Harry Potter", 12, "images/harry-potter.jpg", "images/thumbnails/thumbnail-harry-potter.jpeg", 1), 
+ (2, 6, "A mythology book nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis", "Percy Jackson", 13, "images/percy-jackson.jpg", "images/thumbnails/thumbnail-percy-jackson.jpeg", 1), 
+ (2, 6, "A book with a horrible movie nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis", "Artemis Fowl", 14, "images/artemis-fowl.jpg", "images/thumbnails/thumbnail-artemis-fowl.jpeg", 1), 
+ (2, 6, "A dystopian book nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis", "Divergent", 15, "images/divergent.jpg", "images/thumbnails/thumbnail-divergent.jpeg", 1);
